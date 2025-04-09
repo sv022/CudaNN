5c5f3f9c2be9d7a501230c2ef31daeec2198b7b0
sv022 <ssvatkin04@gmail.com>
2025-04-09 10:09:50 +0300

    feat: add progress bar for training

---
+++ utils/progressbar.h
@@ -0,0 +1,115 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <stdio.h>
+#include <string>
+
+class ProgressBar
+{
+	public:
+		/* Takes in a char for filling up the bar and the size fo the bar */
+		ProgressBar(char notDoneChar, char doneChar, unsigned int size);
+		void end();
+		
+		unsigned int todo;
+		unsigned int done;
+		
+		/* Fills the bar upto a given number */
+		void fillUpCells(unsigned int cells);
+		
+		/* Fills the bar one by one */
+		void fillUp();
+		
+		/* Displays the percentage beside the bar */
+		void displayPercentage();
+	
+		
+		/* Shows tasks done out of the tasks to be done */
+		void displayTasksDone();
+
+		/* Returns the size of the progress bar */
+		unsigned int getSize();
+
+	private:
+		unsigned int size = 0;
+		unsigned int pos = 1;
+		char c;
+		char ch;
+		std::vector <char> bar;
+};
+
+
+/* Defining the constructor */
+ProgressBar::ProgressBar(char notDoneChar, char doneChar, unsigned int size)
+:c(doneChar), ch(notDoneChar), size(size), todo(0), done(0)
+{
+	if(size <= 100)
+	{
+		size = size;	
+	}
+	else{
+		size = 100;
+	}
+	bar.push_back('[');
+	for(int i = 1; i < size + 1; i++)
+	{
+		bar.push_back(ch);
+	}
+
+	bar.push_back(']');
+}
+
+/* Defining fillUpCells */
+void ProgressBar::fillUpCells(unsigned int cells)
+{
+	pos = 0;
+	for(int i = 1; i < cells; i++)
+	{
+		bar[i] = c;
+		std::cout << '\r';
+		for(int j = 0; j < bar.size(); j++)
+		{
+			std::cout << bar[j] << std::flush;
+		}
+	}
+	pos += cells;
+}
+
+/* Defining fillUp */
+void ProgressBar::fillUp()
+{
+	bar[pos] = c;
+	pos++;
+	
+	std::cout << '\r';
+	
+	for(int i = 0; i < bar.size(); i++)
+	{
+		std::cout << bar[i] << std::flush;
+	}
+}
+
+/* Displays the percentage beside the bar */
+void ProgressBar::displayPercentage()
+{
+	float percent = ((float)pos / (float)(bar.size() - 1)) * 100;
+	std::cout << (int)percent << "%";
+}
+
+/* Shows tasks done out of the tasks to be done */
+void ProgressBar::displayTasksDone()
+{
+	std::cout << '(' << done << '/' << todo << ')' << std::flush;
+}
+
+/* Returns the size of the progress bar */
+
+unsigned int ProgressBar::getSize()
+{
+	return bar.size() - 2;
+}
+
+void ProgressBar::end()
+{
+	std::cout << std::endl;
+}
