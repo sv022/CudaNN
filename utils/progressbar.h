fde04eb0d1fcd8a36c50adc900db7efe27b1e5bb
sv022 <ssvatkin04@gmail.com>
2025-05-14 21:44:00 +0300

    chore: clean up code

--- utils/progressbar.h
+++ utils/progressbar.h
@@ -7,58 +7,50 @@
 
 class ProgressBar
 {
-	public:
-		ProgressBar(char notDoneChar, char doneChar, unsigned int size);
-		void end();
-		
-		unsigned int todo;
-		unsigned int done;
-		std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-		
-		void fillUpCells(unsigned int cells);
-		void fillUp();
-		void displayPercentage();
-		void displayTasksDone();
-		void displayTimeElapsed();
+public:
+	ProgressBar(char notDoneChar, char doneChar, unsigned int size);
+	void end();
+	
+	unsigned int todo;
+	unsigned int done;
+	std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+	
+	void fillUpCells(unsigned int cells);
+	void fillUp();
+	void displayPercentage();
+	void displayTasksDone();
+	void displayTimeElapsed();
 
-	private:
-		unsigned int size = 0;
-		unsigned int pos = 1;
-		char c;
-		char ch;
-		std::vector <char> bar;
+private:
+	unsigned int size = 0;
+	unsigned int pos = 1;
+	char c;
+	char ch;
+	std::vector <char> bar;
 };
 
 
 ProgressBar::ProgressBar(char notDoneChar, char doneChar, unsigned int size)
-:c(doneChar), ch(notDoneChar), size(size), todo(0), done(0)
-{
+:c(doneChar), ch(notDoneChar), size(size), todo(0), done(0) {
 	if(size <= 100)
-	{
 		size = size;	
-	}
-	else{
+	else
 		size = 100;
-	}
+
 	bar.push_back('[');
-	for(int i = 1; i < size + 1; i++)
-	{
-		bar.push_back(ch);
-	}
+
+	for(int i = 1; i < size + 1; i++) bar.push_back(ch);
 
 	bar.push_back(']');
 }
 
 void ProgressBar::fillUpCells(unsigned int cells) {
 	pos = 0;
-	for(int i = 1; i < cells; i++)
-	{
+	for(int i = 1; i < cells; i++) {
 		bar[i] = c;
 		std::cout << '\r';
-		for(int j = 0; j < bar.size(); j++)
-		{
-			std::cout << bar[j] << std::flush;
-		}
+
+		for(int j = 0; j < bar.size(); j++) std::cout << bar[j] << std::flush;
 	}
 	pos += cells;
 }
@@ -69,7 +61,7 @@ void ProgressBar::fillUp() {
 	
 	std::cout << '\r';
 	
-	for(int i = 0; i < bar.size(); i++) {
+	for (int i = 0; i < bar.size(); i++) {
 		std::cout << bar[i] << std::flush;
 	}
 }
